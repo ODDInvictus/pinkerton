6d40ac3f9398e671975b032c3445e613d710ff32
Nierot <git@nierot.com>
2022-11-12 22:32:01 +0100

    👌 IMPROVE: Cleanup imports

--- ibs/activity/views.py
+++ ibs/activity/views.py
@@ -1,11 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import permissions
 
 from django.db import transaction
 
