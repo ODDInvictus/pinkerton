c339e33bddbe2cfb6108d31e8105e66be390971a
Nierot <git@nierot.com>
2022-11-12 20:45:07 +0100

    👌 IMPROVE: Switch to function based views instead of class based

--- ibs/activity/urls.py
+++ ibs/activity/urls.py
@@ -5,7 +5,21 @@ from ibs.activity import views
 app_name = 'activity'
 
 urlpatterns = [
-  path('', views.ActivityViewSet.as_view(), name='activity'),
-  path('<int:activity_id>/', views.ActivityDetailViewSet.as_view(), name='activity-detail'),
+  path(
+    '', 
+    views.activity, 
+    name='activity'),
+  path(
+    'create/',
+    views.create_activity,
+    name='create_activity'),
+  path(
+    '<int:activity_id>/', 
+    views.activity_detail, 
+    name='activity-detail'),
+  path(
+    'update/<int:activity_id>/',
+    views.update_activity,
+    name='update-activity'),
   path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- ibs/activity/views.py
+++ ibs/activity/views.py
@@ -2,85 +2,99 @@ from rest_framework.views import APIView
 from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework import permissions
 
+from django.db import transaction
+
 from .serializers import ActivitySerializer
-from .models import Activity
+from .models import Activity, Participant
 
-class ActivityViewSet(APIView):
-  """
-  API endpoint that allows activities to be viewed or edited.
-  """
-  permission_classes = [permissions.IsAuthenticated]
-  queryset = Activity.objects.all().order_by('-date')
+from ibs.users.models import User
+from ibs.tools.permissions import IsSenate, IsSuperAdmin, IsMember
 
-  def get(self, request):
-    serializer = ActivitySerializer(self.queryset.all(), many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def activity(request):
+  if request.method == 'GET':
+    """
+    Returns all activities
+    """
+    activities = Activity.objects.all()
+    serializer = ActivitySerializer(activities, many=True)
+    return Response(serializer.data)
 
-  def post(self, request):
-    data = {
-      'activity': request.data.get('activity') 
-    }
-    serializer = ActivitySerializer(data=data)
+
+@api_view(['POST'])
+@permission_classes([IsMember])
+@transaction.atomic
+def create_activity(request):
+  if request.method == 'POST':
+    """
+    Creates a new activity
+    """
+    serializer = ActivitySerializer(data=request.data)
     if serializer.is_valid():
-      serializer.save()
-      # Next create a Participant object for each person
-      
-      # TODO
+      try:
+        # Run everything in a transaction
+        with transaction.atomic():
+          users = User.objects.all()
+          serializer.save()
 
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
+          # Create a Participant object for every user 
+          for u in users:
+            participant = Participant(user=u, activity=serializer.instance, present=False)
+            participant.save()
+    
+          return Response(serializer.data, status=status.HTTP_201_CREATED)
+      except Exception as e:
+        # If something goes wrong, the transaction is reverted and the error is returned
+        return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ActivityDetailViewSet(APIView):
-  permission_classes = [permissions.IsAuthenticated]
 
-  def get_obj(self, activity_id):
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def activity_detail(request, activity_id):
+  if request.method == 'GET':
+    """
+    Get a specific activity by id
+    """
     try:
-      return Activity.objects.get(pk=activity_id)
+      activity = Activity.objects.get(id=activity_id)
+      serializer = ActivitySerializer(activity)
+      return Response(serializer.data)
     except Activity.DoesNotExist:
       return Response(status=status.HTTP_404_NOT_FOUND)
+  
 
-  def get(self, request, activity_id):
-    activity = self.get_obj(activity_id)
-    if not activity:
-      return Response(
-        {"error": "Activity with id: {} does not exist".format(activity_id)},
-        status=status.HTTP_404_NOT_FOUND
-      )
-
-    serializer = ActivitySerializer(activity)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-  def delete(self, request, activity_id):
-    activity = self.get_obj(activity_id)
-    if not activity:
-      return Response(
-        {"error": "Activity with id: {} does not exist".format(activity_id)},
-        status=status.HTTP_404_NOT_FOUND
-      )
-
-    activity.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-  def patch(self, request, activity_id):
+@api_view(['PATCH', 'DELETE'])
+@permission_classes([IsSenate, IsSuperAdmin])
+def update_activity(request, activity_id):
+  if request.method == 'PATCH':
     """
     Update an activity
     """
-    activity = self.get_obj(activity_id)
+    try:
+      activity = Activity.objects.get(id=activity_id)
+      serializer = ActivitySerializer(activity, data=request.data, partial=True)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Activity.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
 
-    print(vars(activity))
-
-    if not activity:
-      return Response(
-        {"error": "Activity with id: {} does not exist".format(activity_id)},
-        status=status.HTTP_404_NOT_FOUND
-      )
-
-    serializer = ActivitySerializer(activity, data=request.data, partial=True)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_200_OK)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  elif request.method == 'DELETE':
+    """
+    Delete a specific activity by id
+    """
+    try:
+      activity = Activity.objects.get(id=activity_id)
+      activity.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+    except Activity.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
