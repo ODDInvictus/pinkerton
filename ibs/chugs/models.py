1223383321a34654599b2c21d0bb990dc1a5c5e2
Nierot <git@nierot.com>
2022-11-13 19:44:55 +0100

    📦 NEW: Add models for 'strafbakken' and chug timers

--- ibs/chugs/models.py
+++ ibs/chugs/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from ibs.users.models import User
+
+
+# I'm not even going to try to translate that one
+class Strafbak(models.Model):
+  receiver = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Ontvanger', related_name='stafbak_receiver')
+  giver = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Gever', related_name='strafbak_giver')
+  reason = models.CharField(max_length=512, verbose_name='Reden')
+
+  class Meta:
+    verbose_name = 'Strafbak'
+    verbose_name_plural = 'Strafbakken'
+
+  def __str__(self):
+    return f'Strafbak voor {self.receiver} van {self.giver} wegens {self.reason}'
+
+
+class Anytimer(models.Model):
+  receiver = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Ontvanger', related_name='anytimer_receiver')
+  giver = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Gever', related_name='anytimer_giver')
+  reason = models.CharField(max_length=512, verbose_name='Reden')
+
+  class Meta:
+    verbose_name = 'Anytimer'
+    verbose_name_plural = 'Anytimers'
+
+  def __str__(self):
+    return f'Anytimer voor {self.receiver} van {self.giver} wegens {self.reason}'
+
+
+class Chug(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Gebruiker')
+  time = models.FloatField(verbose_name='Tijd')
+  date = models.DateField(verbose_name='Datum')
+  time = models.TimeField(verbose_name='Tijdstip')
+
+  class Meta:
+    verbose_name = 'Chug'
+    verbose_name_plural = 'Chugs'
+
+  def __str__(self):
+    return f'{self.user.get_full_name()} trok een bak in {self.time} op {self.date} om {self.time}'
