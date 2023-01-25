bb711c2a38b792a3c0a17e67042df5221d1fbce0
Niels <git@nierot.com>
2023-01-25 19:48:58 +0100

    cringe

--- ibs/maluspunten/form.py
+++ ibs/maluspunten/form.py
@@ -6,14 +6,14 @@ from ibs.users.models import User, CommitteeMember
 class MaluspuntForm(forms.Form):
   # TODO: dit weghalen of ModelChoiceField gaan gebruiken
   # helemaal kut als je migraties moet doen
-  feuten = CommitteeMember.objects.filter(committee__abbreviation=settings.COMMITTEE_ABBREVIATION_ASPIRING_MEMBER)
-  members = CommitteeMember.objects.filter(committee__abbreviation=settings.COMMITTEE_ABBREVIATION_MEMBER)
-  user = forms.ChoiceField(
-    label='Feut',
-    choices=[(x.user.id, x.user.get_full_name()) for x in feuten])
-  added_by = forms.ChoiceField(
-    label='Toegevoegd door',
-    choices=[(x.user.id, x.user.get_full_name()) for x in members])
+  # feuten = CommitteeMember.objects.filter(committee__abbreviation=settings.COMMITTEE_ABBREVIATION_ASPIRING_MEMBER)
+  # members = CommitteeMember.objects.filter(committee__abbreviation=settings.COMMITTEE_ABBREVIATION_MEMBER)
+  # user = forms.ChoiceField(
+    # label='Feut',
+    # choices=[(x.user.id, x.user.get_full_name()) for x in feuten])
+  # added_by = forms.ChoiceField(
+    # label='Toegevoegd door',
+    # choices=[(x.user.id, x.user.get_full_name()) for x in members])
   reason = forms.CharField(max_length=1024, label='Reden')
   amount = forms.IntegerField(label='Aantal maluspunten', initial=1)
   
